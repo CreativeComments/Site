a76d3b56b150e57d3c59e04a472de718a8cb6c51
Tijs Verkoyen <tijs@verkoyen.eu>
2012-09-25 15:06:48 +0200

    If already logged in we shouldn't show the register/login-page

--- modules/users/actions/login.php
+++ modules/users/actions/login.php
@@ -26,6 +26,20 @@ class UsersLogin extends SiteBaseAction
 	 */
 	public function execute()
 	{
+		// redirect if a user is already logged in
+		if($this->currentUser != null)
+		{
+			// grab the url if provided
+			$url = SpoonFilter::getGetValue('redirect', null, '/' . $this->url->getLanguage());
+
+			// add a report
+			if(substr_count($url, '?') > 0) $url .= '&report=logged-in';
+			else $url .= '?report=logged-in';
+
+			// redirect
+			$this->redirect($url);
+		}
+
 		// load the form
 		$this->loadForm();
 
@@ -96,8 +110,15 @@ class UsersLogin extends SiteBaseAction
 			{
 				Authentication::login($user);
 
+				// grab the url if provided
+				$url = SpoonFilter::getGetValue('redirect', null, '/' . $this->url->getLanguage());
+
+				// add a report
+				if(substr_count($url, '?') > 0) $url .= '&report=logged-in';
+				else $url .= '?report=logged-in';
+
 				// redirect
-				$this->redirect(SpoonFilter::getGetValue('redirect', null, '/'));
+				$this->redirect($url);
 			}
 		}
 	}
--- modules/users/actions/register.php
+++ modules/users/actions/register.php
@@ -18,6 +18,20 @@ class UsersRegister extends SiteBaseAction
 	 */
 	public function execute()
 	{
+		// redirect if a user is already logged in
+		if($this->currentUser != null)
+		{
+			// grab the url if provided
+			$url = SpoonFilter::getGetValue('redirect', null, '/' . $this->url->getLanguage());
+
+			// add a report
+			if(substr_count($url, '?') > 0) $url .= '&report=logged-in';
+			else $url .= '?report=logged-in';
+
+			// redirect
+			$this->redirect($url);
+		}
+
 		$this->display();
 	}
 }
