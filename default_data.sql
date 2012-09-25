7166fd4b852bba7a93f927538696ce55cb548ae5
Tijs Verkoyen <tijs@verkoyen.eu>
2012-09-25 16:51:05 +0200

    Default dump

--- default_data.sql
+++ default_data.sql
@@ -8,12 +8,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `data` text NOT NULL,
   `created_on` datetime NOT NULL,
   `edited_on` datetime DEFAULT NULL,
-  `last_login_on` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-INSERT INTO `users` (`id`, `name`, `password`, `email`, `secret`, `type`, `data`, `created_on`, `edited_on`, `last_login_on`) VALUES
-(1, 'Sumocoders', '57bf7fd2449834188ee1497644294a27c93f0faa', 'accounts@sumocoders.be', 'b6b8968b9ffc307e727fe3a437d967db', 'admin', 'a:1:{s:8:"settings";a:0:{}}', '2012-06-20 13:37:00', NULL, NULL);
+INSERT INTO `users` (`id`, `name`, `password`, `email`, `secret`, `type`, `data`, `created_on`, `edited_on`) VALUES
+(1, 'Sumocoders', '57bf7fd2449834188ee1497644294a27c93f0faa', 'accounts@sumocoders.be', 'b6b8968b9ffc307e727fe3a437d967db', 'admin', 'a:1:{s:8:"settings";a:0:{}}', '2012-06-20 13:37:00', NULL);
 
 CREATE TABLE IF NOT EXISTS `users_sessions` (
   `session_id` varchar(255) NOT NULL,
