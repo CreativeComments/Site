9e5f7e525f8f4acee2cb7e0d97eaf46a84a1ea0e
Niels De Paepe <niels@sumocoders.be>
2013-02-22 15:33:38 +0100

    added disclaimer page and content

---
+++ modules/pages/actions/disclaimer.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * ExampleIndex
+ *
+ * @package		pages
+ * @subpackage	index
+ *
+ * @author 		Tijs Verkoyen <tijs@sumocoders.be>
+ * @since		1.0
+ */
+class PagesDisclaimer extends SiteBaseAction
+{
+	/**
+	 * Execute the action
+	 *
+	 * @return void
+	 */
+	public function execute()
+	{
+
+		// parse
+		$this->parse();
+
+		// display the page
+		$this->display();
+	}
+
+	/**
+	 * Parse the page
+	 *
+	 * @return void
+	 */
+	private function parse()
+	{
+	}
+}
