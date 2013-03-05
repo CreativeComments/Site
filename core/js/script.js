706ac30b76a438f98f9a8218f5bde8d4c9047ca5
Tijs Verkoyen <tijs@verkoyen.eu>
2013-03-05 17:19:37 +0100

    No need for a check if the elements are available as they will only bind if there are elements. Thx to Sam

--- core/js/script.js
+++ core/js/script.js
@@ -448,20 +448,18 @@ jsSite.creativeComments =
 			});
 		});
 
-		if($('#emotionFilter a').length > 0) {
-			$('#emotionFilter a').on('click', function(e) {
-				var emotion = $(this).data('value');
-				var $items = $('.item .' + emotion).parents('.item');
+		$('#emotionFilter a').on('click', function(e) {
+			var emotion = $(this).data('value');
+			var $items = $('.item .' + emotion).parents('.item');
 
-				if($($items[0]).is(':visible')) {
-					$items.fadeOut();
-				} else {
-					$items.fadeIn();
-				}
+			if($($items[0]).is(':visible')) {
+				$items.fadeOut();
+			} else {
+				$items.fadeIn();
+			}
 
-				$(this).parent('li').toggleClass('active');
-			});
-		}
+			$(this).parent('li').toggleClass('active');
+		});
 	}
 }
 
