d14bb0f364dc62cf632326e2da67308a65f0470b
Niels De Paepe <niels@sumocoders.be>
2013-03-15 10:05:06 +0100

    removed parse code for business and team page

--- modules/pages/actions/team.php
+++ modules/pages/actions/team.php
@@ -18,67 +18,6 @@ class PagesTeam extends SiteBaseAction
 	 */
 	public function execute()
 	{
-		$this->parse();
 		$this->display();
-	}
-
-	/**
-	 * Parse the page
-	 *
-	 * @return void
-	 */
-	private function parse()
-	{
-		$this->parseMostRecent();
-	}
-
-	private function parseMostRecent()
-	{
-		$mostRecent = CommentsHelper::getMostRecent();
-
-		// if there are no recent items we shouldn't do anything
-		if(empty($mostRecent)) return false;
-
-		$userIds = array();
-
-		foreach($mostRecent as $row)
-		{
-			$userIds[] = $row->userId;
-		}
-
-		$userIds = array_unique($userIds);
-
-		$userData = Site::getDB()->getRecords(
-			'SELECT i.*, UNIX_TIMESTAMP(i.created_on) AS created_on, UNIX_TIMESTAMP(i.edited_on) AS edited_on
-				 FROM users AS i
-				 WHERE i.id IN (' . implode(', ', $userIds) . ')'
-		);
-
-		$users = array();
-		foreach($userData as $row)
-		{
-			$user = new User();
-			$user->initialize($row);
-			$users[$row['id']] = $user;
-		}
-
-		$items = array();
-		$i = 1;
-		foreach($mostRecent as $comment)
-		{
-			$data = $comment->toArray();
-			$data['user'] = $users[$comment->userId]->toArray();
-			$data['newRow'] = false;
-			if($i == 5) {
-				$data['showAdd'] = true;
-				$i++;
-			}
-			if($i % 3 == 0) $data['newRow'] = true;
-
-			$items[] = $data;
-			$i++;
-		}
-
-		$this->tpl->assign('items', $items);
 	}
 }
