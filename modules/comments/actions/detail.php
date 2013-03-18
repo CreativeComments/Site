5a234a14308a3dcbaf3d6b5946e89ae40ccc98a0
Tijs Verkoyen <tijs@verkoyen.eu>
2013-03-18 10:12:58 +0100

    Open graph tags

--- modules/comments/actions/detail.php
+++ modules/comments/actions/detail.php
@@ -42,6 +42,26 @@ class CommentsDetail extends SiteBaseAction
 	 */
 	private function parse()
 	{
+		$title = SiteTemplateModifiers::truncate($this->item->getText(), 20, false);
+		if(mb_strlen($this->item->getText()) > 20) $title .= '...';
+		if($title == '') $title = 'Creative Comments video message - click for more...';
+		$url = SITE_URL . $this->item->getFullUrl();
+		$imageUrl = SITE_URL; // . $this->item->getOGImageUrl();
+
+		$content = SiteTemplateModifiers::truncate($this->item->getText(), 255, false);
+		if(mb_strlen($this->item->getText()) > 255) $content . '...';
+		if($content == '') $content = 'Click title to watch video message';
+
+		// build meta
+		$meta = '<meta property="og:title" content="' . $title . '">' . "\n";
+		$meta .= '<meta property="og:type" content="website">' . "\n";
+		$meta .= '<meta property="og:url" content="' . $url . '">' . "\n";
+		$meta .= '<meta property="og:image" content="' . $imageUrl . '">' . "\n";
+		$meta .= '<meta property="og:site_name" content="Creative Comments - Spice-up the conversation!">' . "\n";
+		$meta .= '<meta property="og:description" content="' . $content . '">' . "\n";
+
+		$this->tpl->assign('meta', $meta);
+
 		$user = User::get($this->item->getUserId());
 
 		// @todo	parse meta/open graph data
