769876ec268d7b5effc4206d003f16a2785bda7c
Tijs Verkoyen <tijs@verkoyen.eu>
2013-05-06 17:47:44 +0200

    Revert "Flip the image horizontally"
    This reverts commit 21272de8f2a43479e2759ac673a8bc8a087271b5.

--- modules/api/actions/recorder_settings.php
+++ modules/api/actions/recorder_settings.php
@@ -63,8 +63,6 @@ class ApiRecorderSettings extends SiteBaseAction
 		$config['streamPrefix'] = '';
 		$config['streamName'] = '';
 
-		$config['flipImageHorizontally'] = 'true';
-
 		echo 'donot=removethis';
 		foreach ($config as $key => $value){
 			echo '&'.$key.'='.$value;
