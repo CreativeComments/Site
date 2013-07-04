0a8a725f1aee094ace144f2fde37b42a44a75593
Tijs Verkoyen <tijs@verkoyen.eu>
2013-07-04 17:06:49 +0200

    Added method to store still

---
+++ modules/api/actions/video_still.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * ApiVideoStill
+ *
+ * @package		api
+ * @subpackage	video_still
+ *
+ * @author 		Tijs Verkoyen <tijs@sumocoders.be>
+ * @since		1.0
+ */
+class ApiVideoStill extends SiteBaseAction
+{
+	/**
+	 * Execute the action
+	 *
+	 * @return void
+	 */
+	public function execute()
+	{
+		$videoId = SpoonFilter::getGetValue('video', null, '');
+		if($videoId == '') exit;
+
+		$id = (int) Site::getDB()->getVar(
+			'SELECT id
+			FROM comments
+			WHERE i.video_id = ?',
+			$videoId
+		);
+
+		if($id != 0)
+		{
+			$filename = Site::getFilename() . '.jpg';
+			$path = Site::getFilesPath($filename);
+			SpoonFile::setContent($path, $GLOBALS['HTTP_RAW_POST_DATA']);
+
+			Site::getDB(true)->update(
+				'comments',
+				array('video_still' => $filename),
+				'id = ?',
+				array($id)
+			);
+		}
+		exit;
+	}
+}
